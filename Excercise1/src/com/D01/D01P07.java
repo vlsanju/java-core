6d7332fc2d8bd32ce6e20ca0391b06546331f134
vlsanju <sanjanahanumakonda05@gmail.com>
2024-04-04 12:58:35 +0530

    Update D01P07.java

--- Excercise1/src/com/D01/D01P07.java
+++ Excercise1/src/com/D01/D01P07.java
@@ -1,1 +1,29 @@
+package com.D01;
+ 
+import java.util.Scanner;
+ 
+public class D01P07 {
+
+	public static void main(String[] args) {
+		//int percentage = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the percentage to check the grade");
+		int grade = sc.nextInt();
+		if( grade >= 60) {
+			System.out.println("Grade A");
+		}
+		else if (grade >= 45) {
+			System.out.println("Grade B");
+		}
+		else if (grade >=35) {
+			System.out.println("Grade C");
+		}
+		else {
+			System.out.println("Fail");
+		}
+		sc.close();
+	}
+
+}
+
 
